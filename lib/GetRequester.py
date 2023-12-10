2a96b5a1e4e1741a3735c789b8391412ddfa1e0a
BookmDan <danoh329@gmail.com>
2023-12-10 15:28:09 -0800

    complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,21 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
-
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            return response.content
+        except requests.RequestException as e:
+            print(f"error fetching response: {e}")
+            return None
+        
     def load_json(self):
-        pass
+        data = self.get_response_body()
+        if data:
+            try:
+                return json.loads(data)
+            except json.JSONDecodeError as e:
+                # Handle JSON decoding errors
+                print(f"Error decoding JSON: {e}")
+                return None
+        return None
